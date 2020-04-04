49386b39af1b142710f71cad04b54de660f2b792
coderzstas <coderz35@gmail.com>
2020-04-04 20:22:08 +0300

    soong : add more tools to paths whitelist
    fixes : build logspam like
    17:16:28 Disallowed PATH tool "python3.6" used: []string{"/var/lib/jenkins/crdroid/out/.path/python3.6", "/var/lib/jenkins/crdroid/.repo/repo/main.py", "--repo-dir=/var/lib/jenkins/crdroid/.repo", "--wrapper-version=2.4", "--wrapper-path=/usr/bin/repo", "--", "forall", "-pc", "git log --oneline --after=$After_Date --until=$Until_Date"}
    See https://android.googlesource.com/platform/build/+/master/Changes.md#PATH_Tools for more information.
    
    Change-Id: I42c81507bd88ea5435b3e199d6ff5cb83b46af83

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -99,7 +99,14 @@ var Configuration = map[string]PathConfig{
 	"numfmt":   Allowed,
 	"openssl":  Allowed,
 	"patch":    Allowed,
+	"pkg-config": Allowed,
 	"pstree":   Allowed,
+	"python3":  Allowed,
+	"python3.6":  Allowed,
+	"python3.7":  Allowed,
+	"python3.8":  Allowed,
+	"realpath": Allowed,
+	"repo":     Allowed,
 	"rsync":    Allowed,
 	"sh":       Allowed,
 	"tput":     Allowed,
@@ -119,7 +126,6 @@ var Configuration = map[string]PathConfig{
 	"ld":         Forbidden,
 	"ld.bfd":     Forbidden,
 	"ld.gold":    Forbidden,
-	"pkg-config": Forbidden,
 
 	// These are toybox tools that only work on Linux.
 	"pgrep": LinuxOnlyPrebuilt,
