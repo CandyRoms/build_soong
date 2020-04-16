2e5420cbd09cc875eaa4edc712255dc0f3ea0f92
Dan Cartier <nospamdan1@gmail.com>
2020-04-15 22:16:56 -0400

    config: Whitelist tput for colors
    Change-Id: Iacf701408cecfadedda3f2f6a612d05717b78fd6

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -93,6 +93,7 @@ var Configuration = map[string]PathConfig{
 	"pstree":  Allowed,
 	"rsync":   Allowed,
 	"sh":      Allowed,
+	"tput":    Allowed,
 	"tr":      Allowed,
 	"unzip":   Allowed,
 	"zip":     Allowed,
