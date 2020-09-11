fc7029c0e5df406cbc69d451a361a1dacade7e2c
dianlujitao <dianlujitao@lineageos.org>
2020-09-11 11:41:51 +0800

    soong: Make sure to preserve candy product variables
    Change-Id: Ice4093b86baed6974bab630f0e80f6b13a5e28f3

--- android/variable.go
+++ android/variable.go
@@ -594,6 +594,12 @@ func createVariableProperties(moduleTypeProps []interface{}, productVariables in
 func createVariablePropertiesType(moduleTypeProps []interface{}, productVariables interface{}) reflect.Type {
 	typ, _ := proptools.FilterPropertyStruct(reflect.TypeOf(productVariables),
 		func(field reflect.StructField, prefix string) (bool, reflect.StructField) {
+			if strings.HasPrefix(prefix, "Product_variables.Candy") {
+				// Convert Product_variables.Candy.Foo to Candy.Foo
+				_, prefix = splitPrefix(prefix)
+			}
+
+			// Filter function, returns true if the field should be in the resulting struct
 			if prefix == "" {
 				// Keep the top level Product_variables field
 				return true, field
